26b521f6a56529bbfe4962a72d79443c00b05651
Ben S <shaw.j.benjamin@gmail.com>
2020-06-25 14:45:09 +0000

    Add product count, category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
   def show
   end
+
+  def product_count
+    @product_count = Product.count
+  end
+  helper_method :product_count
+
+  def category_count
+    @category_count = Category.count
+  end
+  helper_method :category_count
 end
